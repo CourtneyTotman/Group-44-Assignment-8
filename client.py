8bed9f73f3daae364e163450734e50387e20d849
CourtneyTotman <courtney.totman@student.csulb.edu>
2024-11-27 14:54:37 -0800

    Updated client to include menu for user, and sends to server

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
 
 import socket
 
+def menu(): 
+    print("Please enter a number 1--4")
+    print("1. What is the average moisture inside my kitchen fridge in the past three hours?")
+    print("2. What is the average water consumption per cycle in my smart dishwasher?")
+    print("3. Which device consumed more electricity among my loT devices (two refrigerators and a dishwasher)?")
+    print("4. Exit\n")
+
+
 def main():
     print("----------BEGINNING CLIENT----------\n")
 
     try:
         #User input server IP and Port
         print("INPUT SERVER INFORMATION")
+
         server_ip = str(input("Enter the server IP address: "))
         server_port = int(input("Enter the server port number: "))
         print()
@@ -22,34 +31,51 @@ def main():
             print(f"Connected to ip: {server_ip}")
             print(f"Connected to port: {server_port} \n")
             
+            #while user wants query
             while True:
-                #user inputs message to send to server
-                message = input("ENTER A MESSAGE TO SEND TO THE SERVER (type 'exit' to quit):")
-                print()
+                #user is asked to see menu
+                user_repeat = input("Would you like to see the menu? (y or n)")
+                while (user_repeat != 'y' and user_repeat != 'Y' and user_repeat != 'n' and user_repeat != 'N'):
+                    #user must enter y or n
+                    user_repeat = input("Please enter either y or n")
+                if user_repeat == 'y' or user_repeat == 'Y':
+                    menu()
+                elif user_repeat == 'n' or user_repeat == 'N':
+                    print()
+                
 
-                #if user inputs 'exit' program will close
-                if message.lower() == 'exit':
-                    print("YOU ENTERED EXIT\nCLOSING CONNECTION\n")
+                #get user choice
+                user_choice = int(input("ENTER A NUMBER: "))
+
+                #menu choice : exit
+                if user_choice == 4: 
                     break
+
+                #incorrect menu choice ( outside of 1 - 4 )
+                elif user_choice != 1 and user_choice != 2 and user_choice != 3: 
+                    print("Incorrect selection, please try again")
+
+                #user chose a number between 1 - 3
+                else:
+                    #send server user choice
+                    client_socket.send(bytearray(str(user_choice), encoding='utf-8'))
+                    #wait for server response. then recieve from 3015
+                    server_response = client_socket.recv(3015)
+                    #print server response
+                    print(f"SERVER RESPONSE: {server_response.decode('utf-8')}")                
                 
-                #client socket sends message
-                client_socket.send(bytearray(str(message), encoding='utf-8'))
-                
-                #server responds with capitalized message
-                new_message = client_socket.recv(3015)
-                print(f"SERVER HAS RESPONDED!\nServer response: {new_message.decode('utf-8')}\n")
 
             #close socket once user is done
             client_socket.close()
-            print("CONNECTION CLOSED\n")
+            print("\nCONNECTION CLOSED\n")
         
         #if client cannot connect to server, throw exception
         except (socket.gaierror, socket.error) as e:
-            print(f"ERROR: Server unable to connect. {e}\n")
+            print(f"\nERROR: Server unable to connect. {e}\n")
     
     #if user inputs invalid port number, have user try again
     except ValueError:
-        print("ERROR: Invalid port number\nPlease enter a valid integer.\n")
+        print("\nERROR: Invalid port number\nPlease enter a valid integer.\n")
 
     print("----------END OF CLIENT----------\n")
 
